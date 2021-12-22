95b928a9d9f33d8f4fd001755c87082ecf9f2915
jchuang02 <jchuangsa@gmail.com>
2021-12-21 21:16:02 -0500

    Implement react routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
 
 const App = () => {
-  return <div>Hello there</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<StreamList />} />
+          <Route path="streams/new" element={<StreamCreate />} />
+          <Route path="streams/edit" element={<StreamEdit />} />
+          <Route path="streams/delete" element={<StreamDelete />} />
+          <Route path="streams/show" element={<StreamShow />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
