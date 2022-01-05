33ae22aca68dae21af8f745dd0bef74fd71cbceb
Jonathan Chuang <jchuangsa@gmail.com>
2022-01-04 22:22:19 -0500

    Implement reducers for streams

---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,26 @@
+/* eslint-disable import/no-anonymous-default-export */
+import _ from "lodash";
+import {
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  CREATE_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    default:
+      return state;
+  }
+};
